6b208647d327686a0ee972205822dd1f2ee5ab8a
Shayekhzaman <ashikzaman149@gmail.com>
2022-05-14 01:55:06 +0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
+import QRCode from 'react-native-qrcode-svg';
+import Logo4 from './assets/Logo4.png';
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [qrValue, setQrValue] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <QRCode
+        value={qrValue ?qrValue : 'NA'}
+        size={290}
+        color="red"
+        backgroundColor="black"
+        logo = {Logo4}
+        logoSize={50}
+        logoMargin = {2}
+        logoBorderRadius = {15}
+        logoBackgroundColor = "black"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => {
+          setInput(text);
+        }}
+      />
+      <TouchableOpacity onPress={() =>{setQrValue(input)}}>
+        <Text>Generate Qr Codes</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +35,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  input:{
+    // width: 70,
+    height: 70
+  }
 });
